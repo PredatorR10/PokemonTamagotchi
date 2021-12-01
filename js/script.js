9d9f3ef137e36c8197f8dfdcb515740c4d89cabd
PredatorR10 <godofgamingpr10@gmail.com>
2021-12-01 00:56:54 -0500

    Game start button code added, warning message for invalid name added

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ let introScreen = $("#intro");
 let gameScreen = $("#game");
 let gameOverScreen = $("#gameOver");
 
+//Game variables
+let age = $("#pokeAge");
+let name = $("#pokeName");
+let level = $("#pokeLevel");
+
+let currentAge = 1;
+let currentLevel = 1;
+
 //Screens start hidden, fade in intro on page load
 introScreen.fadeIn();
 
@@ -21,3 +29,18 @@ function gameReset() {
     gameOverScreen.fadeOut();
     setTimeout(() => { introScreen.fadeIn(); }, 1500);
 };
+
+//Start game button
+$("#startButton").on("click", function() {
+    $("#nameWarning").text("");     //Clear warning message if present
+    if($("#nameField").val() !== "") {      //Checks for an input that isnt blank
+        //initialize default game status and open main game page
+        name.text($("#nameField").val());
+        level.text(currentLevel);
+        age.text(currentAge);
+        gameStart();
+    } else {
+        $("#nameWarning").append("Please Enter A Valid Name!");     //Display warning if input is blank
+    };
+});
+
